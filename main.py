4fc0239d7dfbbc47e42a44113afba1abfbcd3cb1
Maksim Mislavskii <48388102+mislavskii@users.noreply.github.com>
2025-06-17 16:31:19 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot.version
+import telebot
+
+from auth import *
+
+print('Hello, zerocoders!')
+print('telebot version:', telebot.version.__version__)
+
+bot = telebot.TeleBot(token)
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     if "привет" in message.text.lower():
+#         bot.send_message(message.chat.id, "Ты со мной noздоровался?")
+#     else:
+#         bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(commands=[ 'start' ])
+def send_welcome(message):
+	bot.reply_to(message, """\
+Привет! Я эхо-бот!
+Умею повторять за тобой!!! \
+""")
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if "привет" in message.text.lower():
+        bot.send_message(message.chat.id, "Ты со мной noздоровался?")
+    else:
+        bot.send_message(message.chat.id, message.text)
+
+
+bot.infinity_polling()
