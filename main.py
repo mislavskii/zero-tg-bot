2e134e442422685282812822f1e8f0a64bce507c
Maksim Mislavskii <48388102+mislavskii@users.noreply.github.com>
2025-06-17 16:38:03 +0000

    bot drafted  and running

--- main.py
+++ main.py
@@ -8,18 +8,6 @@ print('telebot version:', telebot.version.__version__)
 
 bot = telebot.TeleBot(token)
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-
-# @bot.message_handler(func=lambda message: True)
-# def echo_message(message):
-#     if "привет" in message.text.lower():
-#         bot.send_message(message.chat.id, "Ты со мной noздоровался?")
-#     else:
-#         bot.send_message(message.chat.id, message.text)
-
-
 @bot.message_handler(commands=[ 'start' ])
 def send_welcome(message):
 	bot.reply_to(message, """\
